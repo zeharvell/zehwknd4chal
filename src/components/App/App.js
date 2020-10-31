b88dfc5ade5436600bcb30a084ea1492ad940311
Zane Harvell <zane.harvell@gmail.com>
2020-10-31 14:18:20 -0500

    mostrec

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,51 @@
+import axios from 'axios';
 import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  state = {
+    galleryItem: {
+      id: '',
+      path: '',
+      description: '',
+      likes: '',
+    },
+    galleryList: [],
+    errorMsg: null,
+  };
+
+  componentDidMount() {
+    //getdata
+  
+    this.getGalleryData();
+  }
+  
+  getGalleryData() {
+    axios({
+      method: 'GET',
+      url: '/gallery',
+    })
+    .then((response) => {
+      console.log('getGalleryData', this.state.galleryList);
+      this.setState({
+        galleryList: response.data
+      });
+    })
+    .catch((error) => {
+      console.log(error);
+      this.setState({
+        errorMsg: 'ERror in GET',
+      });
+    });
+
+    updateGalleryData() {
+      axios({
+        method: 'PUT',
+        url: '/like/:id',
+        data: 'empty',
+      })
+    }
+  
   render() {
     return (
       <div className="App">
