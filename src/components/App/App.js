fa95ea3aac385e57e216c19b7b743e23b017330a
Zane Harvell <zane.harvell@gmail.com>
2020-10-31 14:56:27 -0500

    sorta update wrkn

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -16,45 +16,56 @@ class App extends Component {
 
   componentDidMount() {
     //getdata
-  
+
     this.getGalleryData();
   }
-  
+
   getGalleryData() {
     axios({
       method: 'GET',
       url: '/gallery',
     })
-    .then((response) => {
-      console.log('getGalleryData', this.state.galleryList);
-      this.setState({
-        galleryList: response.data
-      });
-    })
-    .catch((error) => {
-      console.log(error);
-      this.setState({
-        errorMsg: 'ERror in GET',
-      });
-    });
-
-    updateGalleryData() {
-      axios({
-        method: 'PUT',
-        url: '/like/:id',
-        data: 'empty',
+      .then((response) => {
+        console.log('getGalleryData', response.data);
+        this.setState({
+          galleryList: response.data,
+        });
       })
-    }
-  
+      .catch((error) => {
+        console.log(error);
+        this.setState({
+          errorMsg: 'ERror in GET',
+        });
+      });
+  }
+  updateGalleryData() {
+    axios({
+      method: 'PUT',
+      url: '/like/:id',
+      data: 'empty',
+    });
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Gallery of my life</h1>
         </header>
-        <br/>
+        <br />
         <p>Gallery goes here</p>
-        <img src="images/goat_small.jpg"/>
+        <img src="images/goat_small.jpg" />
+        {JSON.stringify(this.state.galleryList)}
+
+        <div>
+          {this.state.galleryList.map((item) => (
+            <div key={item.id}>
+              <img src={item.path} />
+              <p>{item.description}</p>
+              <p>{item.likes} people love this!</p>
+            </div>
+          ))}
+        </div>
       </div>
     );
   }
